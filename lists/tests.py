836ef06683d51fa95b6f137d478db37b676f6fb4
Allen <allenarcenal@gmail.com>
2016-06-16 00:16:06 +0800

    first unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 # Create your tests here.
 
 
-class SampleTest(TestCase):
+class HomepageTest(TestCase):
 
-    def test_math(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
 
